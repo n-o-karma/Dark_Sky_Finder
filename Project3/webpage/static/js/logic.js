443f2702d34fd2764d1331ef47f7c49a45788512
n-o-karma <nickok@comcast.net>
2023-10-17 18:07:43 -0400

    some webpage updates

--- Project3/webpage/static/js/logic.js
+++ Project3/webpage/static/js/logic.js
@@ -1,10 +1,28 @@
-const lightpollu_path = '../api_data/Resources/lightpollution_v2.csv'
+let lightpollu_path = '../api_data/Resources/lightpollution_v2.csv'
 d3.csv(lightpollu_path).then(createLayers);
 
-// const moonphase_path = '../api_data/Resources/moon_phases.csv'
-// d3.csv(moonphase_path).then(findDates);
+function createLayers(response) {
+  let myMarkers = [];
+  let heatArray = [];
 
-function createMap(skymarkers, heatMap){
+  for (let i = 0; i < response.length; i++) {
+    let event = response[i];
+    if (event.NELM >= 4){
+      let myMark = L.marker([event.Latitude, event.Longitude]).bindPopup(`<h2> ${event.State}, </h2> <h2> NELM ${event.NELM} </h2> `);
+      myMarkers.push(myMark);
+      heatArray.push([event.Latitude, event.Longitude]);
+    }
+  }
+
+    let overlayMaps = {
+      "Data Sites": L.layerGroup(myMarkers),
+      'Heat Map': L.heatLayer(heatArray,{minOpacity:0.35,maxZoom:10})
+    };
+    L.control.layers(baseMaps,overlayMaps, {
+      collapsed: false
+    }).addTo(map);
+}
+
 let Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
 	minZoom: 0,
 	maxZoom: 20,
@@ -12,43 +30,49 @@ let Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/a
 	ext: 'png'
 });
 
+let map = L.map("map", {
+  center: [37.0902, -95.7129],
+  zoom: 4,
+  layers: [Stadia_AlidadeSmoothDark]
+});
+
 let baseMaps = {
-    "Street Map": Stadia_AlidadeSmoothDark
-  };
+  "Street Map": Stadia_AlidadeSmoothDark
+};
 
-  let overlayMaps = {
-    "Data Sites": skymarkers,
-    'Heat Map': heatMap
-  };
+function zoomIn(state){
+  let geoapify_url = `https://api.geoapify.com/v1/geocode/search?state=${state}&type=state&country=United%20States%20of%20America.&format=geojson&apiKey=${geoapify_key}`
+  d3.json(geoapify_url).then(stateZoom)
 
-  let map = L.map("map", {
-    center: [37.0902, -95.7129],
-    zoom: 5,
-    layers: [Stadia_AlidadeSmoothDark,skymarkers]
-  });
-
-  L.control.layers(baseMaps,overlayMaps, {
-    collapsed: false
-  }).addTo(map);
+  function stateZoom(response){
+    for (item of response.features){
+      if (item.properties.state_code == state){
+        stateCoords = [item.properties.lat, item.properties.lon]
+        map.flyTo(stateCoords,6)
+      }
+      else{break}
+    }
+  }
 }
 
-function createLayers(response) {
-    let myMarkers = [];
-    let heatArray = [];
+let moonphase_path = '../api_data/Resources/moon_phases.csv'
+d3.csv(moonphase_path).then(findDates);
 
-    for (let i = 0; i < response.length; i++) {
-      let event = response[i];
-;
-
-      if (event.NELM >= 4){
-        heatArray.push([event.Latitude, event.Longitude]);
-        let myMark = L.marker([event.Latitude, event.Longitude]).bindPopup(event.State);
-        myMarkers.push(myMark)
-      }
-    }
-    createMap(L.layerGroup(myMarkers), L.heatLayer(heatArray,{minOpacity:0.35,maxZoom:10}));
+function findDates(response){
+  console.log(response)
+  labels = []
+  for (let i = 1; i < 13; i++){
+    labels.push(response.columns[i])
   }
-
-function zoomIn(){
-
+  const theChart = document.getElementById('myChart');
+  new Chart(theChart,{
+    type:'line',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Moon Visibility',
+        data: [response]
+      }]
+    }
+  })
 }
