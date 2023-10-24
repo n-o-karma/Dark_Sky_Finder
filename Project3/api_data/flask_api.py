0aa73669463525202f72eb3597d98efc6df9ca14
desireetrevino <dzz_@MacBook-Pro-de-dzz.local>
2023-10-24 12:44:46 -0600

    Flask working

--- Project3/api_data/flask_api.py
+++ Project3/api_data/flask_api.py
@@ -1,47 +1,92 @@
-import numpy as np
-
+# Import the dependencies.
+from flask import Flask, jsonify
 import sqlalchemy
+import os
+import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import numpy as np
 
-from flask import Flask, jsonify
 
+#################################################
+# Database Setup
+#################################################
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+engine = create_engine("sqlite:///final_lightpollution.sqlite")
 
-url = '' #this is the url to the database of joined jsons we end up creating
-engine = create_engine(url)
-
+# reflect an existing database into a new model
 Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
 
-Base.prepare(autoload_with=engine)
+# Save reference to the table
+Lightpoldb = Base.classes.lightpollution
 
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
 
 @app.route("/")
 def welcome():
+    """List all available api routes."""
     return (
-        "Available Routes:<br/>"
-        "/api/v1.0/ROUTE1<br/>"
-        "/api/v1.0/ROUTE2<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/allstates<br/>"
+        #f"/api/v1.0/bystate?state=state"
+        f"/api/v1.0/bystate/state"
     )
 
 
-@app.route("/api/v1.0/ROUTE1")
-def funct():
+@app.route("/api/v1.0/allstates")
+def allstates():
+    # Create our session (link) from Python to the DB
     session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all states
+    results = session.query(Lightpoldb.NELM, Lightpoldb.Constellation, Lightpoldb.State).all()
 
     session.close()
 
-    return jsonify('')
+    # Convert list of tuples into normal list
+    all_states = list(np.ravel(results))
+
+    return jsonify(all_states)
 
 
-@app.route("/api/v1.0/ROUTE2")
-def funct2():
+@app.route(f"/api/v1.0/bystate/<state>")
+def bystate(state):
+    print('state')
+    print(state)
+    print('state')
+    # Create our session (link) from Python to the DB
     session = Session(engine)
-    
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Lightpoldb.Latitude,Lightpoldb.Longitude, Lightpoldb.NELM, Lightpoldb.Constellation, Lightpoldb.State).filter(Lightpoldb.State == state).all()
+
     session.close()
 
-    return jsonify('')
+    # Create a dictionary from the row data and append to a list of all_passengers
+    filtered_db = []
+    for latitude, longitude, nelm, constellation, state in results:
+        lightpol_dict = {}
+        lightpol_dict["Latitude"] = latitude
+        lightpol_dict["Longitude"] = longitude
+        lightpol_dict["NELM"] = nelm
+        lightpol_dict["Constellation"] = constellation
+        lightpol_dict["State"] = state
+        filtered_db.append(lightpol_dict)
+
+    return jsonify(filtered_db)
 
 
 if __name__ == '__main__':
